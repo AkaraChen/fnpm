91f0f5a71b0ee5039ee9a4bdba056ff8fe0b848b
无尘室内点墨 <akarachen@outlook.com>
2024-09-15 18:22:41 +0800

    fix(ui): development server

--- packages/fnpm-ui/vite.config.ts
+++ packages/fnpm-ui/vite.config.ts
@@ -2,13 +2,15 @@ import { vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
     plugins: [remix(), tsconfigPaths()],
     build: {
         target: 'esnext',
-        minify: true,
+        minify: isProduction,
     },
     ssr: {
-        noExternal: true,
+        noExternal: isProduction || undefined,
     },
 });
