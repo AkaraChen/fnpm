6cefbf2299aa8db5899eb7494d1e55936ddb9e1b
AkaraChen <akarachen@outlook.com>
2025-06-08 16:18:39 +0800

    feat(tailwind): add tailwindcss-animate plugin for enhanced animations chore(tailwind): remove unused tailwind.config.ts file to clean up the project feat(markdown): integrate rehype-starry-night for improved code highlighting fix(globals.css): add missing theme variables for better styling consistency refactor(app-sidebar): simplify Sidebar component by removing unnecessary class feat(knip.json): include config files in entry points for better processing

--- packages/fnpm-dev/src/lib/markdown.ts
+++ packages/fnpm-dev/src/lib/markdown.ts
@@ -1,7 +1,7 @@
-import rehypeShiki from '@shikijs/rehype';
 import rehypeDocument from 'rehype-document';
 import rehypeExternalLinks from 'rehype-external-links';
 import rehypeFormat from 'rehype-format';
+import rehypeStarryNight from 'rehype-starry-night';
 import rehypeStringify from 'rehype-stringify';
 import remarkGfm from 'remark-gfm';
 import remarkParse from 'remark-parse';
@@ -12,15 +12,10 @@ export const processor = unified()
     .use(remarkParse)
     .use(remarkRehype)
     .use(remarkGfm)
-    .use(rehypeShiki, {
-        themes: {
-            light: 'one-light',
-            dark: 'one-dark-pro',
-        },
-    })
     .use(rehypeDocument)
     .use(rehypeFormat)
     .use(rehypeExternalLinks, {
         target: '_blank',
     })
+    .use(rehypeStarryNight)
     .use(rehypeStringify);
