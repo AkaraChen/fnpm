2c6b4160d2cd19b76ddac8ce7e340659849dbb0f
AkaraChen <akarachen@outlook.com>
2025-06-08 22:03:28 +0800

    refactor: extract command mounting logic into separate function for better organization

--- packages/fnpm/src/commands/index.ts
+++ packages/fnpm/src/commands/index.ts
@@ -1,3 +1,21 @@
+import type { Argv } from 'yargs';
+import Add from './add';
+import CI from './ci';
+import Config from './config';
+import Create from './create';
+import Default from './default';
+import Dlx from './dlx';
+import Doctor from './doctor';
+import Init from './init';
+import Publish from './publish';
+import Remove from './remove';
+import Test from './test';
+import UI from './ui';
+import Update from './update';
+import Use from './use';
+import View from './view';
+import Why from './why';
+
 export { default as Add } from './add';
 export { default as CI } from './ci';
 export { default as Config } from './config';
@@ -14,3 +32,23 @@ export { default as Update } from './update';
 export { default as Use } from './use';
 export { default as View } from './view';
 export { default as Why } from './why';
+
+export function mount(argv: Argv) {
+    return argv
+        .command(new Add())
+        .command(new Dlx())
+        .command(new Remove())
+        .command(new Create())
+        .command(new Init())
+        .command(new Test())
+        .command(new CI())
+        .command(new Doctor())
+        .command(new UI())
+        .command(new Default())
+        .command(new Use())
+        .command(new Update())
+        .command(new Publish())
+        .command(new View())
+        .command(new Why())
+        .command(new Config());
+}
--- packages/fnpm/src/fnpm.ts
+++ packages/fnpm/src/fnpm.ts
@@ -2,24 +2,7 @@
 
 import yargs from 'yargs';
 import pkg from '../package.json';
-import {
-    Add,
-    CI,
-    Config,
-    Create,
-    Default,
-    Dlx,
-    Doctor,
-    Init,
-    Publish,
-    Remove,
-    Test,
-    UI,
-    Update,
-    Use,
-    View,
-    Why,
-} from './commands';
+import { mount } from './commands';
 import { type Context, getContext } from './util';
 
 const _ctx = await getContext(process.cwd());
@@ -29,29 +12,14 @@ declare global {
     var ctx: Context;
 }
 
-yargs(ctx.args)
-    .scriptName('fnpm')
-    .completion()
-    .recommendCommands()
-    .version(pkg.version)
-    .alias('version', 'v')
-    .help()
-    .alias('help', 'h')
-    .usage('Usage: $0 <command> [options]')
-    .command(new Add())
-    .command(new Dlx())
-    .command(new Remove())
-    .command(new Create())
-    .command(new Init())
-    .command(new Test())
-    .command(new CI())
-    .command(new Doctor())
-    .command(new UI())
-    .command(new Default())
-    .command(new Use())
-    .command(new Update())
-    .command(new Publish())
-    .command(new View())
-    .command(new Why())
-    .command(new Config())
-    .parse();
+mount(
+    yargs(ctx.args)
+        .scriptName('fnpm')
+        .completion()
+        .recommendCommands()
+        .version(pkg.version)
+        .alias('version', 'v')
+        .help()
+        .alias('help', 'h')
+        .usage('Usage: $0 <command> [options]'),
+).parse();
