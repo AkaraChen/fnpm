488a4d22dbab7a579ae3baafe512f90703272069
AkaraChen <akarachen@outlook.com>
2025-06-08 22:14:01 +0800

    fix: correctly parse dlx command arguments and display full command string

--- packages/fnpm/src/commands/dlx.ts
+++ packages/fnpm/src/commands/dlx.ts
@@ -1,23 +1,27 @@
 import consola from 'consola';
 import { commands } from 'pm-combo';
-import type { CommandModule } from 'yargs';
+import type { EmptyObject } from 'type-fest';
+import type { ArgumentsCamelCase, CommandModule } from 'yargs';
 import { error, exec, normalizePackageVersion } from '../util';
 
 class Dlx implements CommandModule {
     public command = 'dlx';
     public describe = 'download and exec';
-    public builder = {};
 
-    public async handler() {
-        const [pkg, ...rest] = globalThis.ctx.args.slice(1);
+    public async handler(args: ArgumentsCamelCase<EmptyObject>) {
+        const pkg =
+            args._[0] === 'dlx' ? (args._[1] as string) : (args._[0] as string);
+        const rest = globalThis.ctx.args.slice(
+            globalThis.ctx.args.indexOf(pkg) + 1,
+        );
         if (!pkg) {
             error('No package specified');
         }
         const command = commands.dlx.concat(globalThis.ctx.pm, {
-            package: normalizePackageVersion(pkg!),
+            package: normalizePackageVersion(pkg),
             args: rest,
         });
-        consola.info(`Running ${command}`);
+        consola.info(`Running ${command.join(' ')}`);
         await exec(command);
     }
 }
