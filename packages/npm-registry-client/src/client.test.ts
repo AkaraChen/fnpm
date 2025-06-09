ba27172d7e79138f5b3b4b73c66cf85562b33b8a
AkaraChen <akarachen@outlook.com>
2025-06-09 14:37:10 +0800

    test: add test for api client

---
+++ packages/npm-registry-client/src/client.test.ts
@@ -0,0 +1,42 @@
+import { describe, expect, it, vi } from 'vitest';
+import { createApiClient } from './client';
+
+describe('createApiClient', () => {
+    it('should create a client', async () => {
+        const mockFetch = vi.fn();
+        const options = {
+            baseUrl: 'https://test.com',
+            fetch: mockFetch,
+        };
+        const client = createApiClient(options);
+        try {
+            // @ts-expect-error
+            await client.GET('/test-url');
+        } catch (e) {
+            expect(e).toBeInstanceOf(Error);
+        }
+        expect(mockFetch).toHaveBeenCalledOnce();
+    });
+
+    it('should merge options with new options', async () => {
+        const mockFetch = vi.fn();
+        const options = {
+            baseUrl: 'https://test.com',
+            fetch: mockFetch,
+        };
+        const client = createApiClient(options);
+        const client2 = client.with({
+            baseUrl: 'https://test2.com',
+            fetch: () => {
+                return mockFetch(1);
+            },
+        });
+        try {
+            // @ts-expect-error
+            await client2.GET('/test-url');
+        } catch (e) {
+            expect(e).toBeInstanceOf(Error);
+        }
+        expect(mockFetch).toHaveBeenCalledWith(1);
+    });
+});
--- packages/npm-registry-client/src/client.ts
+++ packages/npm-registry-client/src/client.ts
@@ -1,3 +1,4 @@
+import { defu } from 'defu';
 import createClient, { type Client, type ClientOptions } from 'openapi-fetch';
 
 export type ApiClient<path extends {}> = Client<path> & {
@@ -6,6 +7,8 @@ export type ApiClient<path extends {}> = Client<path> & {
 
 export const createApiClient = <path extends {}>(options: ClientOptions) => {
     const client = createClient<path>(options) as ApiClient<path>;
-    client.with = createApiClient;
+    client.with = (inputOptions: ClientOptions) => {
+        return createApiClient(defu(inputOptions, options));
+    };
     return client;
 };
