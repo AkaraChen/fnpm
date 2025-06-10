247c9876081889c5aca2220a109fabcbafdcd037
AkaraChen <akarachen@outlook.com>
2025-06-10 13:11:06 +0800

    feat: increase test timeout in CI environment using is-ci package

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
+import isCi from 'is-ci';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     test: {
         projects: ['packages/*'],
+        testTimeout: isCi ? 5_000 * 100 : undefined,
     },
 });
