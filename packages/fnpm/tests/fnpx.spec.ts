c7030e31f09a9331f0e18cbb95a5d4c7f23c47ae
AkaraChen <akarachen@outlook.com>
2025-06-10 14:25:45 +0800

    chore: move timeout config before test function in fnpx spec

--- packages/fnpm/tests/fnpx.spec.ts
+++ packages/fnpm/tests/fnpx.spec.ts
@@ -26,38 +26,32 @@ describe('fnpx smoke test', () => {
         );
     });
 
-    it(
-        'should install and run',
-        async () => {
-            const program = Effect.gen(function* () {
-                const executor = yield* CommandExecutor.CommandExecutor;
-                const fs = yield* FileSystem.FileSystem;
-                const dir = yield* fs.makeTempDirectoryScoped();
-                const command = pipe(
-                    Command.make(
-                        'node',
-                        executable,
-                        'create-next-app@15.3.3',
-                        'app-name',
-                        ' --empty',
-                        '--yes',
-                    ),
-                    Command.workingDirectory(dir),
-                    Command.runInShell(true),
-                );
-                const appDir = path.join(dir, 'app-name');
-                const process = yield* executor.start(command);
-                const code = yield* process.exitCode;
-                expect(code).toBe(0);
-                const exists = yield* fs.exists(appDir);
-                expect(exists).toBe(true);
-            });
-            await Effect.runPromise(
-                pipe(program, Effect.provide(NodeContext.layer), Effect.scoped),
+    it('should install and run', { timeout: 5_000 * 100 }, async () => {
+        const program = Effect.gen(function* () {
+            const executor = yield* CommandExecutor.CommandExecutor;
+            const fs = yield* FileSystem.FileSystem;
+            const dir = yield* fs.makeTempDirectoryScoped();
+            const command = pipe(
+                Command.make(
+                    'node',
+                    executable,
+                    'create-next-app@15.3.3',
+                    'app-name',
+                    ' --empty',
+                    '--yes',
+                ),
+                Command.workingDirectory(dir),
+                Command.runInShell(true),
             );
-        },
-        {
-            timeout: 5_000 * 100,
-        },
-    );
+            const appDir = path.join(dir, 'app-name');
+            const process = yield* executor.start(command);
+            const code = yield* process.exitCode;
+            expect(code).toBe(0);
+            const exists = yield* fs.exists(appDir);
+            expect(exists).toBe(true);
+        });
+        await Effect.runPromise(
+            pipe(program, Effect.provide(NodeContext.layer), Effect.scoped),
+        );
+    });
 });
