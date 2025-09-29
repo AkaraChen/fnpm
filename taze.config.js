add6eb0f67c3847968300e3ef7e636cb35aa54a4
AkaraChen <akarachen@outlook.com>
2025-09-29 20:27:59 +0800

    build: update deps

--- taze.config.js
+++ taze.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'taze';
 
 export default defineConfig({
-    exclude: ['@pnpm/types'],
+    exclude: [
+        '@pnpm/types',
+        '@effect/platform',
+        '@effect/platform-node',
+        'effect',
+    ],
     force: true,
     write: true,
+    recursive: true,
+    update: true,
 });
